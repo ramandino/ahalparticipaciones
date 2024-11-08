77ec3baa2de0d70cd5dedb18b14032d0c6149d3f
Ramiro Andino <ramiroandino987@gmail.com>
2024-11-08 10:07:46 -0500

    cargo corporativo toggle works

--- app/dashboard.js
+++ app/dashboard.js
@@ -1,6 +1,8 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
+    togglefactor = .75
+
 
     let dump_de_participaciones = document.getElementById("dump_de_participaciones")
     let participaciones_compartment = document.getElementById("participacion_compartment")
@@ -9,14 +11,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
     agregar_participaciones_button.addEventListener("click",function(){
 
-        let newParticipacion = participaciones_compartment.content.cloneNode(true);
-
-        let newmonto = newParticipacion.querySelector("[name='montoparticipacion']");
-        let newporcentaje = newParticipacion.querySelector("[name='porcentaje_participacion']");
-        let newsubtotal = newParticipacion.querySelector("#subtotal");
+        let newParticipacion = participaciones_compartment.content.cloneNode(true)
+        let newmonto = newParticipacion.querySelector("[name='montoparticipacion']")
+        let newporcentaje = newParticipacion.querySelector("[name='porcentaje_participacion']")
+        let newsubtotal = newParticipacion.querySelector("#subtotal")
+        let toggle = newParticipacion.querySelector("[name='togglecargo']")
 
         dump_de_participaciones.appendChild(newParticipacion)
-
 
         newmonto.addEventListener("input", function() {
             updateSubtotal(newmonto, newporcentaje, newsubtotal);
@@ -24,28 +25,18 @@ document.addEventListener("DOMContentLoaded", function() {
         newporcentaje.addEventListener("input", function() {
             updateSubtotal(newmonto, newporcentaje, newsubtotal);
         });
+        toggle.addEventListener("change", function() {
+            togglefactor = this.checked ? 0.75 : 1
+            updateSubtotal(newmonto, newporcentaje, newsubtotal);
+        });
+
     })
 
     function updateSubtotal(montoElement, porcentajeElement, subtotalElement) {
         let monto = parseFloat(montoElement.value) || 0;
         let porcentaje = parseFloat(porcentajeElement.value) || 0;
-        let subtotalcalculo = (monto * (porcentaje / 100)).toFixed(2);
+        let subtotalcalculo = (monto * togglefactor * (porcentaje / 100)).toFixed(2);
         subtotalElement.textContent = `$ ${subtotalcalculo}`;
     }
-
-    // function updateSubtotal(){
-    //     let monto = parseFloat(document.getElementsByName("montoparticipacion").value)|| 0;
-    //     let porcentaje = parseFloat(document.getElementsByName("porcentaje_participacion").value) || 0;
-    //     let subtotalcalculo = (monto * (porcentaje / 100)).toFixed(2);
-
-    //     subtotal.textContent =`$ ${subtotalcalculo}`
-    // }
-
-    // document.getElementsByName("montoparticipacion").addEventListener("input", updateSubtotal());
-    // document.getElementsByName("porcentaje_participacion").addEventListener("input", updateSubtotal());
-
-
-
-
 
 })
