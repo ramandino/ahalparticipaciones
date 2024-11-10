9f8220df8c77176bbac17193db8c2090239def27
Ramiro Andino <ramiroandino987@gmail.com>
2024-11-10 13:20:46 -0500

    subtotal final working

--- app/dashboard.js
+++ app/dashboard.js
@@ -2,11 +2,12 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     togglefactor = .75
-
+    let subtotal = 0
 
     let dump_de_participaciones = document.getElementById("dump_de_participaciones")
     let participaciones_compartment = document.getElementById("participacion_compartment")
     let agregar_participaciones_button = document.getElementById("agregar_participacion")
+    let totales_compartment = document.querySelector("#totales")
 
 
     agregar_participaciones_button.addEventListener("click",function(){
@@ -26,17 +27,43 @@ document.addEventListener("DOMContentLoaded", function() {
             updateSubtotal(newmonto, newporcentaje, newsubtotal);
         });
         toggle.addEventListener("change", function() {
-            togglefactor = this.checked ? 0.75 : 1
+            togglefactor = this.checked ? 0.75 : 1 
             updateSubtotal(newmonto, newporcentaje, newsubtotal);
         });
 
+
+        // aqui es donde va todo lo de subtotales
+        totales_compartment.classList.remove("hidden")
+
     })
+
+
+    // sumatoria de subtotales
+
+    let sumatoriasubtotales = document.querySelector("#sumatoriasubtotales")
 
     function updateSubtotal(montoElement, porcentajeElement, subtotalElement) {
         let monto = parseFloat(montoElement.value) || 0;
         let porcentaje = parseFloat(porcentajeElement.value) || 0;
         let subtotalcalculo = (monto * togglefactor * (porcentaje / 100)).toFixed(2);
-        subtotalElement.textContent = `$ ${subtotalcalculo}`;
+        let subtotalcalculonumber = numeral(subtotalcalculo)
+        let subtotalfinal = numeral(subtotalcalculonumber).format("0,") 
+        subtotalElement.textContent = `$ ${subtotalfinal}`;
+        calculateGrandTotal();
+    }
+
+    function calculateGrandTotal() {
+        let grandTotal = 0;
+        
+        
+        // Loop through all subtotals in dump_de_participaciones and sum them up
+        dump_de_participaciones.querySelectorAll("#subtotal").forEach(subtotalElement => {
+            let subtotalValue = parseInt(subtotalElement.textContent.replace(/[^0-9]+/g,"")) || 0;
+            grandTotal += subtotalValue;
+        });
+
+        // Update the grand total display
+        sumatoriasubtotales.textContent = `$ ${numeral(grandTotal).format("0,")}`;
     }
 
 })
