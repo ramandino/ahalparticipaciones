72c92186ef19dc78ddd9da9cc0fe1e85e7892b0a
Ramiro Andino <ramiroandino987@gmail.com>
2024-11-11 10:07:12 -0500

    total final working falta salario fijo

--- app/dashboard.js
+++ app/dashboard.js
@@ -2,13 +2,16 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     togglefactor = .75
-    let subtotal = 0
 
     let dump_de_participaciones = document.getElementById("dump_de_participaciones")
     let participaciones_compartment = document.getElementById("participacion_compartment")
     let agregar_participaciones_button = document.getElementById("agregar_participacion")
+    let sumatoriasubtotales = document.querySelector("#sumatoriasubtotales")
     let totales_compartment = document.querySelector("#totales")
+    let iva = document.querySelector("#montoiva")
+    let isr = document.querySelector("#montoisr")
 
+    // cuando se da click a agregar
 
     agregar_participaciones_button.addEventListener("click",function(){
 
@@ -30,18 +33,12 @@ document.addEventListener("DOMContentLoaded", function() {
             togglefactor = this.checked ? 0.75 : 1 
             updateSubtotal(newmonto, newporcentaje, newsubtotal);
         });
-
-
-        // aqui es donde va todo lo de subtotales
+        // aqui es donde se display el subtotal compartment
         totales_compartment.classList.remove("hidden")
-
     })
 
 
     // sumatoria de subtotales
-
-    let sumatoriasubtotales = document.querySelector("#sumatoriasubtotales")
-
     function updateSubtotal(montoElement, porcentajeElement, subtotalElement) {
         let monto = parseFloat(montoElement.value) || 0;
         let porcentaje = parseFloat(porcentajeElement.value) || 0;
@@ -49,21 +46,43 @@ document.addEventListener("DOMContentLoaded", function() {
         let subtotalcalculonumber = numeral(subtotalcalculo)
         let subtotalfinal = numeral(subtotalcalculonumber).format("0,") 
         subtotalElement.textContent = `$ ${subtotalfinal}`;
-        calculateGrandTotal();
+        calculateGrandTotal(subtotalfinal);
     }
 
+    // calcula el subtotal final
     function calculateGrandTotal() {
-        let grandTotal = 0;
-        
-        
-        // Loop through all subtotals in dump_de_participaciones and sum them up
+        let grandSubTotal = 0;
+        // Loops en todos los subtotales de dump_de_participaciones
         dump_de_participaciones.querySelectorAll("#subtotal").forEach(subtotalElement => {
             let subtotalValue = parseInt(subtotalElement.textContent.replace(/[^0-9]+/g,"")) || 0;
-            grandTotal += subtotalValue;
+            grandSubTotal += subtotalValue ;
+            calculo(grandSubTotal)
         });
-
-        // Update the grand total display
-        sumatoriasubtotales.textContent = `$ ${numeral(grandTotal).format("0,")}`;
+        // esto actualiza el sub total final
+        sumatoriasubtotales.textContent = `$ ${numeral(grandSubTotal).format("0,")}`;
     }
 
+
+    // esta funcion envia el calculo para realizar el total
+    function calculo (subtotal) {
+        // IVA
+        calculateTotal(subtotal,iva,isr)
+        iva.addEventListener("input", function() {
+            calculateTotal(subtotal,iva,isr);
+        });
+        // ISR
+        calculateTotal(subtotal,iva,isr)
+        isr.addEventListener("input", function() {
+            calculateTotal(subtotal,iva,isr) ;
+        });
+    }
+
+    // funcion para print el total
+    function calculateTotal(total,ivaelement,isrelement){
+        let totalcompartment = document.querySelector("#total")
+        let ivavalor = ivaelement.value || 0
+        let isrvalor = isrelement.value || 0
+        let totalFinal = total - ivavalor - isrvalor
+        totalcompartment.textContent = `$ ${numeral(totalFinal).format("0,")}`;
+    };
 })
