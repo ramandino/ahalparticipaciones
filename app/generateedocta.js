af15dea1148b714f99a772fa3660c5ac423ef5a2
Ramiro Andino <ramiroandino987@gmail.com>
2024-11-19 20:33:13 -0500

    generación del estado de cuenta js

---
+++ app/generateedocta.js
@@ -0,0 +1,25 @@
+
+// import { dashboard, getSalario } from "./dashboard.js";
+// dashboard()
+
+// const salario = getSalario()
+
+
+let subtotaledo = document.getElementById("subtotaledo")
+let ivaedo = document.getElementById("ivaedo")
+let isredo = document.getElementById("isredo")
+let totaledo = document.getElementById("totaledo")
+
+let colaboradoredo = document.getElementById("colaboradoredo")
+let correoedo = document.getElementById("correoedo")
+let fechaedo = document.getElementById("fechaedo")
+let montoedo = document.getElementById("montoedo")
+
+// donde ira el dump de participaciones
+let dumpedo = document.getElementById("dumpedo")
+
+let print = document.querySelector("#totalbtn")
+
+print.addEventListener("click", function(){
+    console.log(salario)
+})
