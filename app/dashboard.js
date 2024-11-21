a818fd9230e109149eed308b99647e0c8d6d49d8
Ramiro Andino <ramiroandino987@gmail.com>
2024-11-20 23:03:08 -0500

    hasta qui funciona el print y todo ya resolvi lo del array que estaba en 0,tambien no se estan enviando los valores del subtotal

--- app/dashboard.js
+++ app/dashboard.js
@@ -1,103 +1,183 @@
 
-document.addEventListener("DOMContentLoaded", function() {
 
-    togglefactor = .75
-
-    let dump_de_participaciones = document.getElementById("dump_de_participaciones")
-    let participaciones_compartment = document.getElementById("participacion_compartment")
-    let agregar_participaciones_button = document.getElementById("agregar_participacion")
-    let sumatoriasubtotales = document.querySelector("#sumatoriasubtotales")
-    let totales_compartment = document.querySelector("#totales")
-    let iva = document.querySelector("#montoiva")
-    let isr = document.querySelector("#montoisr")
-    let salariofijo = document.querySelector("#salariofijo")
-
-    // cuando se da click a agregar
-
-    let salario = 0
-
-    salariofijo.addEventListener("input",function(){
-        let salarioint = parseInt(this.value) 
-        salario = salarioint || 0
-        calculartotal()
-
-    })
-
-    agregar_participaciones_button.addEventListener("click",function (){
-
-        let newParticipacion = participaciones_compartment.content.cloneNode(true)
-        let newmonto = newParticipacion.querySelector("[name='montoparticipacion']")
-        let newporcentaje = newParticipacion.querySelector("[name='porcentaje_participacion']")
-        let newsubtotal = newParticipacion.querySelector("#subtotal")
-        let toggle = newParticipacion.querySelector("[name='togglecargo']")
-
-        dump_de_participaciones.appendChild(newParticipacion)
-
-        newmonto.addEventListener("input", function() {
-            updateSubtotal(newmonto, newporcentaje, newsubtotal);
-        });
-        newporcentaje.addEventListener("input", function() {
-            updateSubtotal(newmonto, newporcentaje, newsubtotal);
-        });
-        toggle.addEventListener("change", function() {
-            togglefactor = this.checked ? 0.75 : 1 
-            updateSubtotal(newmonto, newporcentaje, newsubtotal);
-        });
-        // aqui es donde se display el subtotal compartment
-        totales_compartment.classList.remove("hidden")
-
-        calculartotal(subtotal)
-        
-        
-    })
-
-    // sumatoria de subtotales
-    function updateSubtotal(montoElement, porcentajeElement, subtotalElement) {
-        let monto = parseFloat(montoElement.value) || 0;
-        let porcentaje = parseFloat(porcentajeElement.value) || 0;
-        let subtotalcalculo = (monto * togglefactor * (porcentaje / 100)).toFixed(2);
-        let subtotalcalculonumber = numeral(subtotalcalculo)
-        let subtotalfinal = numeral(subtotalcalculonumber).format("0,") 
-        subtotalElement.textContent = `$ ${subtotalfinal}`;
-        calculartotal()
-    }
-
-        // calculateGrandTotal()
-        function calculartotal(){
-
-            let grandSubTotal = 0 + salario
-            // Loops en todos los subtotales de dump_de_participaciones
-            dump_de_participaciones.querySelectorAll("#subtotal").forEach(subtotalElement => {
-                let subtotalValue = parseInt(subtotalElement.textContent.replace(/[^0-9]+/g,"")) || 0;
-                grandSubTotal += subtotalValue ;
-                calculo(grandSubTotal)
-            });
-            // esto actualiza el sub total final
-            sumatoriasubtotales.textContent = `$ ${numeral(grandSubTotal).format("0,")}`;
-
-        }
+let togglefactor = .75
 
 
-    // esta funcion envia el calculo para realizar el total
-    function calculo (subtotal) {
-        // IVA
-        calculateTotal(subtotal,iva,isr)
-        iva.addEventListener("input", function() {
-            calculateTotal(subtotal,iva,isr);
-        });
-        // ISR
-        calculateTotal(subtotal,iva,isr)
-        isr.addEventListener("input", function() {
-            calculateTotal(subtotal,iva,isr) ;
-        });
-    }
+let dump_de_participaciones = document.getElementById("dump_de_participaciones")
+let participaciones_compartment = document.getElementById("participacion_compartment")
+let agregar_participaciones_button = document.getElementById("agregar_participacion")
+let sumatoriasubtotales = document.querySelector("#sumatoriasubtotales")
+let totales_compartment = document.querySelector("#totales")
+let iva = document.querySelector("#montoiva")
+let isr = document.querySelector("#montoisr")
+let salariofijo = document.querySelector("#salariofijo")
 
-    // funcion para print el total
-    function calculateTotal(total,ivaelement,isrelement){
-        let totalcompartment = document.querySelector("#total")
-        let ivavalor = ivaelement.value || 0
-        let isrvalor = isrelement.value || 0
-        let totalFinal = total - ivavalor - isrvalor
-        totalcompartment.textContent = `$ ${numeral(totalFinal).format("0,")}`;
-    };
+let nombrecolaborador = document.querySelector("[name='nombrecola']")
+let emailcolaborador = document.querySelector("[name='emailcola']")
+
+let nombrevalor = ""
+let emailvalor = ""
+export let participacionesData = [];
+
+
+nombrecolaborador.addEventListener("input",function(){
+    nombrevalor = nombrecolaborador.value
 })
+emailcolaborador.addEventListener("input",function(){
+    emailvalor = emailcolaborador.value
+})
+
+export function colaInfo(){
+    return {
+        nombre : nombrevalor,
+        email : emailvalor}
+}
+// cuando se da click a agregar
+
+let salario = 0
+
+salariofijo.addEventListener("input",function(){
+    let salarioint = parseInt(this.value) 
+    salario = salarioint || 0
+    calculartotal()    
+    
+})
+
+export function getSalario() {
+    return salario ;
+}
+
+agregar_participaciones_button.addEventListener("click",function (){
+
+    let newParticipacion = participaciones_compartment.content.cloneNode(true)
+    let newmonto = newParticipacion.querySelector("[name='montoparticipacion']")
+    let newporcentaje = newParticipacion.querySelector("[name='porcentaje_participacion']")
+    let newsubtotal = newParticipacion.querySelector("#subtotal")
+    let toggle = newParticipacion.querySelector("[name='togglecargo']")
+    let proyecto = newParticipacion.querySelector("#nombreproyecto")
+
+    proyecto.addEventListener("input",function(){
+        participationEntry.proyecto = proyecto.value || "";
+
+    })
+
+    let participationEntry = {
+        proyecto: "",
+        subtotal: 0,
+    };
+
+    participacionesData.push(participationEntry);
+    
+
+
+
+
+    dump_de_participaciones.appendChild(newParticipacion)
+
+    newmonto.addEventListener("input", function() {
+        updateSubtotal(newmonto, newporcentaje, newsubtotal,participationEntry);
+    });
+    newporcentaje.addEventListener("input", function() {
+        updateSubtotal(newmonto, newporcentaje, newsubtotal,participationEntry);
+    });
+    toggle.addEventListener("change", function() {
+        togglefactor = this.checked ? 0.75 : 1 
+        updateSubtotal(newmonto, newporcentaje, newsubtotal,participationEntry);
+    });
+    // aqui es donde se display el subtotal compartment
+    totales_compartment.classList.remove("hidden")
+
+
+})
+
+
+
+
+
+
+// export function dump_participaciones (){
+//     return {
+//         proyectonombre : proyecto_EDO,
+//         subtotalproyecto : subtotal_EDO
+        
+//     }
+// }
+
+// sumatoria de subtotales
+function updateSubtotal(montoElement, porcentajeElement, subtotalElement,participationEntry) {
+    let monto = parseFloat(montoElement.value) || 0;
+    let porcentaje = parseFloat(porcentajeElement.value) || 0;
+    let subtotalcalculo = (monto * togglefactor * (porcentaje / 100)).toFixed(2);
+    let subtotalcalculonumber = numeral(subtotalcalculo)
+    let subtotalfinal = numeral(subtotalcalculonumber).format("0,") 
+    participationEntry.subtotal = subtotalfinal
+    subtotalElement.textContent = `$ ${subtotalfinal}`;
+    calculartotal()
+}
+
+    // calculateGrandTotal()
+    function calculartotal(){
+
+        let grandSubTotal = 0 + salario
+        // Loops en todos los subtotales de dump_de_participaciones
+        dump_de_participaciones.querySelectorAll("#subtotal").forEach(subtotalElement => {
+            let subtotalValue = parseInt(subtotalElement.textContent.replace(/[^0-9]+/g,"")) || 0;
+            grandSubTotal += subtotalValue ;
+            calculo(grandSubTotal)
+        });
+        // esto actualiza el sub total final
+        sumatoriasubtotales.textContent = `$ ${numeral(grandSubTotal).format("0,")}`;
+
+
+    }
+
+
+// esta funcion envia el calculo para realizar el total
+function calculo (subtotal) {
+    // IVA
+    calculateTotal(subtotal,iva,isr)
+    iva.addEventListener("input", function() {
+        calculateTotal(subtotal,iva,isr);
+    });
+    // ISR
+    calculateTotal(subtotal,iva,isr)
+    isr.addEventListener("input", function() {
+        calculateTotal(subtotal,iva,isr) ;
+    });
+}
+
+// funcion para print el total
+let ivavalor = 0
+let isrvalor = 0
+let totalFinal = 0
+let total = 0
+
+function calculateTotal(totaledo,ivaelement,isrelement){
+    let totalcompartment = document.querySelector("#total")
+    total = totaledo
+    ivavalor = ivaelement.value || 0
+    isrvalor = isrelement.value || 0
+    totalFinal = total - ivavalor - isrvalor
+    totalcompartment.textContent = `$ ${numeral(totalFinal).format("0,")}`;
+    
+};    
+
+export function totales(){
+    return{
+        iva : ivavalor,
+        isr : isrvalor,
+        subtotal : total,
+        total : totalFinal,
+        
+    }
+}
+
+    
+
+        
+    
+    
+    
+    
+
+
--- app/generateedocta.js
+++ app/generateedocta.js
@@ -1,25 +1,56 @@
 
-// import { dashboard, getSalario } from "./dashboard.js";
-// dashboard()
-
-// const salario = getSalario()
+import { getSalario, colaInfo, totales, participacionesData} from "./dashboard.js";
 
 
-let subtotaledo = document.getElementById("subtotaledo")
-let ivaedo = document.getElementById("ivaedo")
-let isredo = document.getElementById("isredo")
-let totaledo = document.getElementById("totaledo")
+// template que se va a clone
+let aquivaeltemplate = document.getElementById("aquivaeltemplate")
+let template_edo = document.getElementById("bodyestado")
+let print = document.querySelector("#print")
 
-let colaboradoredo = document.getElementById("colaboradoredo")
-let correoedo = document.getElementById("correoedo")
-let fechaedo = document.getElementById("fechaedo")
-let montoedo = document.getElementById("montoedo")
+const salario = getSalario()
+const fecha = new Date().toString()
+const {nombre,email} = colaInfo()
+const {iva,isr,subtotal,total} = totales()
 
-// donde ira el dump de participaciones
-let dumpedo = document.getElementById("dumpedo")
 
-let print = document.querySelector("#totalbtn")
 
 print.addEventListener("click", function(){
-    console.log(salario)
+
+    let new_Template_Edo = template_edo.content.cloneNode(true)
+    let colaboradoredo = new_Template_Edo.querySelector("#colaboradoredo")
+    let correoedo = new_Template_Edo.querySelector("#correoedo")
+    let fechaedo = new_Template_Edo.querySelector("#fechaedo")
+    let salarioedo = new_Template_Edo.querySelector("#montoedo")
+    let subtotaledo = new_Template_Edo.querySelector("#subtotaledo")
+    let ivaedo = new_Template_Edo.querySelector("#ivaedo")
+    let isredo = new_Template_Edo.querySelector("#isredo")
+    let totaledo = new_Template_Edo.querySelector("#totaledo")
+    let dumpedo = new_Template_Edo.querySelector("#dumpedo")
+
+    salarioedo.textContent = `${salario}`
+    colaboradoredo.textContent = `${nombre}`
+    correoedo.textContent = `${email}`
+    fechaedo.textContent = `${fecha}`
+    subtotaledo.textContent = `${subtotal}`
+    ivaedo.textContent = `${iva}`
+    isredo.textContent = `${isr}`
+    totaledo.textContent = `${total}`
+
+    console.log(participacionesData)
+    console.log(dumpedo)
+
+    // participacionesData.forEach(entry => {
+    //     dumpedo.innerHTML += `
+    // <div class="font-normal flex mb-2 ">
+    //     <div class="w-80">${entry.proyecto}</div>
+    //     <div>${entry.subtotal}MXN</div>
+    // </div>`       
+    // })
+    
+    // aquivaeltemplate.appendChild(new_Template_Edo)
+    // let element = document.getElementById("aquivaeltemplate")
+    // html2pdf(element); 
+    // aquivaeltemplate.classList.add("hidden");
 })
+
+
